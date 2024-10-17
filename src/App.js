f9cc31c9c115c9d2d9d0ebbeaf186ec0d0217252
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-17 22:26:40 +0530

    adding new books and displaying book-list

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
         // BAD CODE -- WRONG WAY OF UPDATING 'STATE' IN CASE OF ARRAY or OBJECT
+        /*
         books.push({ id: 123, title: title });
         console.log(books);
         setBooks(books);
+        */
+        const updatedBooks = [
+            ...books,
+            { id: Math.round(Math.random() * 9999), title: title },
+        ];
+        setBooks(updatedBooks);
     };
 
     return (
-        <div>
-            {books.length}
+        <div className="app">
+            <BookList books={books} />
             <BookCreate onCreate={createBook} />
         </div>
     );
