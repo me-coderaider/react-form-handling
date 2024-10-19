ce057350489f5a3cd990a7a155bd4c0f1484a4db
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-20 00:43:00 +0530

    API Client, Axios and creating a book using AXIOS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import { useState } from "react";
+import axios from "axios";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 
 function App() {
     const [books, setBooks] = useState([]);
 
-    const createBook = (title) => {
+    const createBook = async (title) => {
+        const response = await axios.post("http://localhost:3001/books", {
+            title: title,
+        });
+        // console.log(response);
+
+        const updatedBooks = [...books, response.data];
+        setBooks(updatedBooks);
+
+        /*
+        const updatedBooks = [
+            ...books,
+            { id: Math.round(Math.random() * 9999), title: title },
+        ];
+        setBooks(updatedBooks);
+        */
+
         // BAD CODE -- WRONG WAY OF UPDATING 'STATE' IN CASE OF ARRAY or OBJECT
         /*
         books.push({ id: 123, title: title });
         console.log(books);
         setBooks(books);
         */
-        const updatedBooks = [
-            ...books,
-            { id: Math.round(Math.random() * 9999), title: title },
-        ];
-        setBooks(updatedBooks);
     };
 
     const deleteBookById = (id) => {
